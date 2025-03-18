cf87f1a21af6edf45f01e89463f7ef2ce52aef7e
Frederik Reimert <frederikreimert@gmail.com>
2025-03-18 13:46:08 +0100

    cookiecutter repo initialized

---
+++ src/diffsam/train.py
@@ -0,0 +1,10 @@
+from diffsam.model import Model
+from diffsam.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
